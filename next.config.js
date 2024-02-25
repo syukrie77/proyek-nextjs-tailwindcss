49832e6c4bafadb0d8bbb986e08a7722f0d844b5
Syukrie77 <syukrie77@gmail.com>
2024-02-25 17:13:59 +0700

    push pertama

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export'
-}
 
-module.exports = nextConfig
+//const nextConfig = {
+//  output: 'export'
+//}
+
+//module.exports = nextConfig
+
+
+// next.config.js
+
+module.exports = {
+  images: {
+    unoptimized: true,
+  },
+};
